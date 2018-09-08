6ac664009ce90de3fc97a22d90d4d6b9768c37eb
StephenTing <1343439622@qq.com>
2018-09-08 10:56:48 +0800

    update
    Signed-off-by: StephenTing <1343439622@qq.com>

--- SNmarket/data/renting-data.js
+++ SNmarket/data/renting-data.js
@@ -1,21 +1,41 @@
 var local_database = [
   {
+    rid:0,
     img:"/images/one.png",
-    title:"长铺沿河街2房一厅拎包入住",
+    title:"长铺沿河街一室一厅拎包入住",
     condition:"1",
-    area:"45",
+    area:"35",
     roomtype:"长铺",
     address:"长铺一中小学附近270栋",
-    rent:"800"
+    rent:"800",
+    phone:"66666666",
+    date:"2018-08-08",
+    view:"55",
+    payMethod:"押一付一",
+    roomType:"一室一厅",
+    rent:"800",
+    floor:"4/9",
+    position:"南",
+    description:"房间整齐无异味，内设空调电脑暖气。"
   },
   {
+    rid:1,
     img: "/images/one.png",
-    title: "长铺沿河街2房一厅拎包入住",
+    title: "长铺沿河街一厅二室情侣套间",
     condition: "1",
-    area: "35",
+    area: "45",
     roomtype: "长铺",
-    address: "长铺一中小学附近270栋",
-    rent: "900"
+    address: "长铺沿河街123国道绿洲宾馆旁边",
+    rent: "900",
+    phone:"88888888",
+    date: "2018-08-22",
+    view: "115",
+    payMethod: "押一付一",
+    roomType: "一厅两室",
+    rent: "900",
+    floor: "4/11",
+    position: "西",
+    description:"房间里面有空调，电脑桌，有暖气呢!"
   }
 ]
 
